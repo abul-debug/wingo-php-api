8a89def7b13525c8564a5b598c0a2f39dfa91ebe
abul-debug <abulla7686@gmail.com>
2026-08-26 13:33:10 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -12,21 +12,19 @@ module.exports = (req, res) => {
     if (!period || isNaN(period)) {
         return res.status(400).json({
             status: "error",
-            message: "Valid period parameter is required. Example: /api/index?period=1001"
+            message: "Valid period parameter is required. Example: /?period=1001"
         });
     }
 
     try {
         const periodNum = BigInt(period);
 
-        // BIG / SMALL Calculation
         const step1 = periodNum * 23n + 17n;
         const step2 = step1 * step1;
         const value = Number((step2 % 100n) % 10n);
 
         const bigSmall = value >= 5 ? "BIG" : "SMALL";
 
-        // Number Calculation
         const bigNumbers = [8.6, 9.5, 7.8, 6.5, 5.7];
         const smallNumbers = [1.3, 2.4, 3.4, 4.1, 0.2];
         const randomIndex = Math.floor(Math.random() * 5);
